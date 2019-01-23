1ab7d90c1782d006ce73ef41018fced74caba756
Nishant Kumar <nishantdawn01@gmail.com>
2019-01-23 14:57:12 +0530

    freeRTOS for POSIX

---
+++ FreeRTOS_Posix/main_blinky.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+
+static void my_task( void *arg )
+{
+	while(1)
+	{
+		printf("Hello World!\n");
+		vTaskDelay(1000);
+	}
+}
+
+void main_blinky( void )
+{
+	xTaskCreate(my_task,(char*)"my_task",1024,NULL,1,NULL);
+	vTaskStartScheduler();
+}
+
